98fd379508e059d9cec390eb30c38a861e98ec56
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2022-12-15 13:15:30 +0530

    Create Add.java

---
+++ Assignment 14.12.22/Add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Add
+{	
+	public static void main(String[] args)
+	{
+		int a,b,sum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number: ");
+		a = sc.nextInt();
+		System.out.println("Enter the second number: ");
+		b = sc.nextInt();
+		sum = a + b;
+		System.out.println("Sum is: " +sum);
+		
+	}
+}
