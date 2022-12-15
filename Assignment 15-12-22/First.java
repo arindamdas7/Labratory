9720db9bd9cf4ea9a4c0baef40db86bc82d90ea6
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2022-12-15 12:28:54 +0530

    Create First.java

---
+++ Assignment 15-12-22/First.java
@@ -0,0 +1,21 @@
+//wap to print hello 5 times
+
+package basic;
+
+public class First {
+
+	public static void main(String[] args) 
+	{
+		int i; //counter variable
+		for(i=0; i<5; i++) //increment
+		{
+			System.out.println("Hello");
+		}
+		System.out.println();
+		for(i=5; i>0; i--) //decrement
+		{
+			System.out.println("World");
+		}
+	}
+
+}
