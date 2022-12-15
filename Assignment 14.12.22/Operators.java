cd309ded74ca71e5ee42ccaa057c2ae0e2844dae
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2022-12-15 13:18:00 +0530

    Create Operators.java

---
+++ Assignment 14.12.22/Operators.java
@@ -0,0 +1,11 @@
+public class Operators
+{
+	public static void main(String[] args)
+	{
+		int a = 10; 
+		int b = 20;
+		System.out.println(a!=b);
+		/*boolean bool = true;
+		System.out.println(bool==false);*/
+	}
+}
