d05657e8cda1f58f94b10bb5541e35c48fb5d89e
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2022-12-15 13:14:23 +0530

    Create Example.java

---
+++ Assignment 14.12.22/Example.java
@@ -0,0 +1,10 @@
+public class Example
+{
+	public static void main(String[] args)
+	{
+		String firstName = "Arindam";
+		String lastName = "Das";
+		String fullName = firstName + lastName; //single line command
+		System.out.println("Full Name is:"+firstName+" "+lastName);
+	}
+}
