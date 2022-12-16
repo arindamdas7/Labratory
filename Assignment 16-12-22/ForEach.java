4aeb373ede78d8477d7e13d243199afcf4161823
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2022-12-16 14:05:30 +0530

    Create ForEach.java

---
+++ Assignment 16-12-22/ForEach.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class ForEach 
+{
+
+	public static void main(String[] args) 
+	{
+		int arr[] = {11,22,33,44,55};
+		for(int x: arr)
+		{
+			System.out.println(x);
+		}
+	}
+
+}
