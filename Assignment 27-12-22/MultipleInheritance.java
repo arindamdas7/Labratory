b2a9737e88e2f9d09d5bf69dc992aadfd27040a5
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2022-12-27 10:14:15 +0530

    Create MultipleInheritance.java

---
+++ Assignment 27-12-22/MultipleInheritance.java
@@ -0,0 +1,29 @@
+package abstractiondemo;
+interface A
+{
+	public void msg();
+}
+interface B
+{
+	public void msg();
+}
+class C implements A,B
+{
+
+	@Override
+	public void msg()
+	{
+		
+	}
+	
+}
+public class MultipleInheritance
+{
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
