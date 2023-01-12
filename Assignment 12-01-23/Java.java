5182f9129c9bb75dbca7245ec5cc16b52707127b
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2023-01-12 14:05:54 +0530

    Create Java.java

---
+++ Assignment 12-01-23/Java.java
@@ -0,0 +1,54 @@
+package batchCollection;
+
+public class Java
+{
+	private int stdId;
+	private String stdName;
+	private long phone;
+	private String email;
+	public Java() 
+	{
+		super();
+	}
+	public int getStdId()
+	{
+		return stdId;
+	}
+	public void setStdId(int stdId) 
+	{
+		this.stdId = stdId;
+	}
+	public String getStdName()
+	{
+		return stdName;
+	}
+	public void setStdName(String stdName) 
+	{
+		this.stdName = stdName;
+	}
+	public long getPhone()
+	{
+		return phone;
+	}
+	public void setPhone(long phone)
+	{
+		this.phone = phone;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	public Java(int stdId, String stdName, long phone, String email)
+	{
+		super();
+		this.stdId = stdId;
+		this.stdName = stdName;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+}
