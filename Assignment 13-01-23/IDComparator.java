aff3ff5a0214c412a2d82a276753d5adba31ddcb
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2023-01-16 12:30:55 +0530

    Create IDComparator.java

---
+++ Assignment 13-01-23/IDComparator.java
@@ -0,0 +1,16 @@
+package collection;
+import java.util.*;
+public class IDComparator implements Comparator<Student>
+{
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		if(o1.id==o2.id)
+			return 0;
+		else if(o1.id>o2.id)
+			return 1;
+		else
+			return -1;
+	}
+
+}
