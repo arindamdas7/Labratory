1c3f3cb0dae0ebcfcbe91654bc0ce5ff65ef631d
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2023-01-16 12:30:01 +0530

    Create Student.java

---
+++ Assignment 13-01-23/Student.java
@@ -0,0 +1,35 @@
+package collection;
+
+public class Student 
+{
+	int id;
+	String name;
+	int roll;
+	public Student() 
+	{
+		super();
+	}
+	public int getId() 
+	{
+		return id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getRoll() 
+	{
+		return roll;
+	}
+	public Student(int id, String name, int roll) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.roll = roll;
+	}
+	
+	
+	
+	
+	
+}
