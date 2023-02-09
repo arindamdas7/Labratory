a0ebe9bf3cf78f72d3d2c96248613bd85cedc421
Arindam Das <120620829+arindamdas7@users.noreply.github.com>
2023-02-09 10:04:21 +0530

    Create SynchronizationDemo.java

---
+++ Assignment 30-01-23/SynchronizationDemo.java
@@ -0,0 +1,47 @@
+class PrintData{
+    synchronized void multiplication(int n){
+        for(int i =1;i<=5;i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+            try {
+                Thread.sleep(400);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+
+class Thread1 extends Thread{
+    PrintData t;
+
+    public Thread1(PrintData t){
+        this.t=t;
+    }
+    public void run(){
+        t.multiplication(5);
+    }
+}
+
+class Thread2 extends Thread{
+    PrintData t;
+
+    public Thread2(PrintData t){
+        this.t=t;
+    }
+    public void run(){
+        t.multiplication(10);
+    }
+}
+
+public class SynchronizationDemo{
+
+    public static void main(String[] args) {
+        PrintData pd = new PrintData();
+
+        Thread1 t1 = new Thread1(pd);
+        Thread2 t2 = new Thread2(pd);
+
+        t1.start();
+        t2.start();
+    }
+}
